f9aa842e22ff058e513e04b8725676761de84364
aveyal <enfer0666@yahoo.com>
2019-04-22 14:57:47 +0300

    completing numfinder

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -9,7 +9,12 @@ public class NumFinderMain {
         // nf.find(new int[] {4, 25, 7, 9});
 
         // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+//        nf.find(new int[] {4, 3, 2, 1});
+//        nf.find(new int[] {1, 2, 3, 4});
+//        nf.find(new int[] {1, 2, 3, 1});
+//        nf.find(new int[] {1, 1});
+//        nf.find(new int[] {});
+        nf.find(null);
 
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
