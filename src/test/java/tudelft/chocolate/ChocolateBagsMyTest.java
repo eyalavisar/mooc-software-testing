89750106d1b1533acf9ac56638e9234c558eee98
aveyal <enfer0666@yahoo.com>
2019-04-23 17:11:35 +0300

    parametrized bars tests

--- src/test/java/tudelft/chocolate/ChocolateBagsMyTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsMyTest.java
@@ -16,8 +16,26 @@ public class ChocolateBagsMyTest {
     }
 
     @Test
+    public void notEnoughBarsBoundryBig(){
+        int result = bags.calculate(0,2,11);
+        Assertions.assertEquals(-1, result);
+    }
+
+    @Test
+    public void notEnoughBarsBoundrySmall(){
+        int result = bags.calculate(1,0,2);
+        Assertions.assertEquals(-1, result);
+    }
+
+    @Test
     public void bigBars(){
         int result = bags.calculate(2,2,10);
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void bigBarsBoundry(){
+        int result = bags.calculate(0,2,10);
         Assertions.assertEquals(0, result);
     }
 
@@ -28,8 +46,20 @@ public class ChocolateBagsMyTest {
     }
 
     @Test
+    public void smallAndBigBarsBoundry(){
+        int result = bags.calculate(2,2,12);
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
     public void smallBars(){
         int result = bags.calculate(4,4,3);
+        Assertions.assertEquals(3, result);
+    }
+
+    @Test
+    public void smallBarsBoundry(){
+        int result = bags.calculate(3,4,3);
         Assertions.assertEquals(3, result);
     }
 }
---
+++ src/test/java/tudelft/chocolate/ChocolateBagsParametrizedTest.java
@@ -0,0 +1,7 @@
+package tudelft.chocolate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChocolateBagsParametrizedTest {
+    
+}
